a0dc272a810fbeda1404bf34544b9ceeb9ff95e7
Artyom Chegodar <>
2022-10-31 13:03:38 +0300

    refacroring Task_7 RomanConverter

--- LeetCodeTasks/RomanToInt.cs
+++ LeetCodeTasks/RomanToInt.cs
@@ -12,45 +12,37 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
  */
 public class RomanToInt
 {
+    private readonly Dictionary<char, int> digits = new()
+    {
+        { 'I', 1 },
+        { 'V', 5 },
+        { 'X', 10 },
+        { 'L', 50 },
+        { 'C', 100 },
+        { 'D', 500 },
+        { 'M', 1000 }
+    };
     public int GetRomanToInt(string s)
     {
         if (s.Length == 0)
             return 0;
         
         int finalNumber = 0;
-        int lastSymbol = 4000;
+        int lastNumber = 4000;
 
         for (int i = 0; i < s.Length; i++)
         {
-            int number = GetNumberFromSymbol(s[i]);
+            digits.TryGetValue(s[i], out int number);
             
-            if (lastSymbol < number){
-                finalNumber -= lastSymbol;
-                finalNumber += number - lastSymbol;
+            if (lastNumber < number){
+                finalNumber += number - 2 * lastNumber;
             }
             else{
                 finalNumber += number;
-                lastSymbol = number;
+                lastNumber = number;
             }
         }
         
         return finalNumber;
-    }
-    
-    private int GetNumberFromSymbol(char letter)
-    {
-        int value = letter switch
-        {
-            'I' => 1,
-            'V' => 5,
-            'X' => 10,
-            'L' => 50,
-            'C' => 100,
-            'D' => 500,
-            'M' => 1000,
-            _ => 1
-        };
-
-        return value;
     }
 }
