d298f5ae8b567d30cbb26032d4a1499dbf6882f1
MS-CAS\admin <a.chegodar@softteco.com>
2023-02-10 15:01:59 +0300

    added LongestCommonPrefix Task

---
+++ LeetCodeTasks/LongestCommonPrefix.cs
@@ -0,0 +1,52 @@
+﻿/*
+ Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+ */
+using System.Text;
+
+namespace LeetCodeTasks
+{
+    public static class LongestCommonPrefix
+    {
+        public static string GetLongestPrefix(string[] strs)
+        {
+            StringBuilder prefix = new StringBuilder();
+
+            string fstWord = strs[0];
+
+            for (int i = 0; i < fstWord.Length; i++)
+            {
+                char letter = fstWord[i];
+
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (i == strs[j].Length || letter != strs[j][i])
+                    {
+                        return prefix.ToString();
+                    }
+                }
+
+                prefix.Append(letter);
+            }
+
+            return prefix.ToString();
+        }
+    }
+}
--- LeetCodeTasks/Program.cs
+++ LeetCodeTasks/Program.cs
@@ -1,6 +1,7 @@
 ﻿using LeetCodeTasks;
 
 /*Task 1*/
+/*
 TwoSum twoSum = new TwoSum();
 
 var twoSumResult_1 = twoSum.GetTwoSum(new int[]{3,2,4}, 6); // 1, 2
@@ -9,8 +10,9 @@ var twoSumResult_2 = twoSum.GetTwoSum(new int[]{ 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1
 Console.WriteLine("===========Task 1 ==============");
 Console.WriteLine(string.Join(", ", twoSumResult_1));
 Console.WriteLine(string.Join(", ", twoSumResult_2));
-
+*/
 /*Task 2*/
+/*
 RemoveElement removeElement = new();
 var nums_1 = new []{ 0, 1, 2, 2, 3, 0, 4, 2 };
 
@@ -23,8 +25,9 @@ var removeElementResult = removeElement.GetRemoveElement(nums_1, 2);
 Console.WriteLine("After changing array:");
 Console.WriteLine(string.Join(", ", nums_1));  // [0, 0, 1, 3, 4, 2147483647, 2147483647, 2147483647]
 Console.WriteLine($"result: {removeElementResult}"); // 5
-
+*/
 /*Task 3*/
+/*
 RemoveDuplicates removeDuplicates = new();
 var nums_3 = new []{0,0,1,1,1,2,2,3,3,4};
 
@@ -37,8 +40,9 @@ var removeDuplicatesResult = removeDuplicates.GetRemoveDuplicates(nums_3);
 Console.WriteLine($"result: {removeDuplicatesResult}"); // 5
 Console.WriteLine("After changing array:");
 Console.WriteLine(string.Join(", ", nums_3)); // [0,1,2,3,4,_,_,_,_,_]
-
+*/
 /*Task 4*/
+/*
 var nums_4 = new []{1,3,5,6};
 
 InsertPosition insertPosition = new();
@@ -48,18 +52,19 @@ var insPosResult_2 = insertPosition.SearchInsert(nums_4, 5);
 Console.WriteLine("===========Task 4 ==============");
 Console.WriteLine($"result: {insPosResult_1}"); // 1
 Console.WriteLine($"result: {insPosResult_2}"); // 2
-
+*/
 /*Task 5*/
 // var nums_5 = new []{5,2,2,6,5,7,1,9,0,3,8,6,8,6,5,2,1,8,7,9,8,3,8,4,7,2,5,8,9};
+/*
 var nums_5 = new []{9, 9, 9}; 
 PlusOne one = new();
 var plusOneResult = one.GetPlusOne(nums_5);
 
 Console.WriteLine("===========Task 5 ==============");
 Console.WriteLine(string.Join(", ", plusOneResult)); // [1,0,0]
-
+*/
 /*Task 6*/
-var nums_6 = new []{7,1,5,3,6,4};
+/*var nums_6 = new []{7,1,5,3,6,4};
 var nums_6_2 = new []{7,6,4,3,1};
 var nums_6_3 = new []{2,4,1};
 BestTimeBuyAndSellStock stock = new();
@@ -71,8 +76,9 @@ Console.WriteLine("===========Task 6 ==============");
 Console.WriteLine($"result: {stockResult_1}"); // 5
 Console.WriteLine($"result: {stockResult_2}"); // 0
 Console.WriteLine($"result: {stockResult_3}"); // 2
-
+*/
 /*Task 7*/
+/*
 string nums_7 = "LVIII";
 string nums_7_2 = "MCMXCIV";
 RomanToInt roman = new();
@@ -82,8 +88,9 @@ var romanResult_2 = roman.GetRomanToInt(nums_7_2);
 Console.WriteLine("===========Task 7 ==============");
 Console.WriteLine($"result: {romanResult_1}"); // 58
 Console.WriteLine($"result: {romanResult_2}"); // 1994
-
+*/
 /*Task 8*/
+/*
 Parentheses parentheses = new();
 var isValidBrackets_1 = parentheses.IsValid("()[]{}");
 var isValidBrackets_2 = parentheses.IsValid("([])");
@@ -94,8 +101,9 @@ Console.WriteLine($"result: {isValidBrackets_1}"); // true
 Console.WriteLine($"result: {isValidBrackets_2}"); // true
 Console.WriteLine($"result: {isValidBrackets_3}"); // false
 Console.WriteLine($"result: {isValidBrackets_4}"); // false
-
+*/
 /*Task 9*/
+/*
 ValidAnagram anagram = new ValidAnagram();
 var isValidAnagram_1 = anagram.IsAnagram("anagram", "nagaram");
 var isValidAnagram_2 = anagram.IsAnagram("rat", "car");
@@ -104,4 +112,21 @@ Console.WriteLine("===========Task 9 ==============");
 Console.WriteLine($"result: {isValidAnagram_1}"); // true
 Console.WriteLine($"result: {isValidAnagram_2}"); // false
 
+*/
 
+Console.WriteLine("=========== LongestCommonPrefix ==============");
+string[] strs = { "flower", "flow", "flight" };
+string prefix = LongestCommonPrefix.GetLongestPrefix(strs);
+Console.WriteLine(prefix);
+
+string[] str2 = { "flower", "flower", "flower", "flower" };
+string prefix_2 = LongestCommonPrefix.GetLongestPrefix(str2);
+Console.WriteLine(prefix_2);
+
+string[] str3 = { "a", "ac", "ass" };
+string prefix_3 = LongestCommonPrefix.GetLongestPrefix(str3);
+Console.WriteLine(prefix_3);
+
+string[] str4 = { "", "ac" };
+string prefix_4 = LongestCommonPrefix.GetLongestPrefix(str4);
+Console.WriteLine(prefix_4);
