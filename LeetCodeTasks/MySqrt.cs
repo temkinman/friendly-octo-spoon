8d33ab0d9a51f5661db0b2cff03d6f11cfb4914d
Artyom Chegodar <>
2023-02-17 16:23:19 +0300

    Mysqrt

---
+++ LeetCodeTasks/MySqrt.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeTasks;
+
+public static class MySqrt
+{
+	public static int Sqrt(int x)
+	{
+		int number = (int)Math.Sqrt(x);
+
+		//int result = (int)Math.Ceiling(number);
+		return number;
+	}
+}
